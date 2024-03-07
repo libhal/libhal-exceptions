e5b23616eadb7754726e054de1c2ce6169b13275
Khalil Estell <kammce0@gmail.com>
2024-03-07 06:18:53 -0800

    :adhesive_bandage: Use exception control in test_package

--- test_package/main.cpp
+++ test_package/main.cpp
@@ -12,8 +12,11 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+#include <cstdio>
+
 #include <libhal-exceptions/control.hpp>
 
 int main()
 {
+  hal::set_terminate(+[]() { puts("terminating application!"); });
 }
