1fa9196f3e9774c847a92280f30a21a8aed41711
Khalil Estell <kammce0@gmail.com>
2025-01-19 14:17:16 -0800

    near point did work for single levels, now testing multi

--- demos/main.cpp
+++ demos/main.cpp
@@ -1,4 +1,5 @@
 #include <libhal-exceptions/control.hpp>
+#include <new>
 
 #include "resource_list.hpp"
 
@@ -14,4 +15,22 @@ int main()
   hal::set_terminate(terminate_handler);
   auto resources = initialize_platform();
   application(resources);
+}
+
+extern "C"
+{
+
+  [[gnu::section(".text.throw_bad_alloc")]]
+  void __wrap__ZSt17__throw_bad_allocv()
+  {
+    throw std::bad_alloc();
+  }
+
+  [[gnu::section(".text.abort")]]
+  void __wrap_abort()
+  {
+    while (true) {
+      continue;
+    }
+  }
 }
