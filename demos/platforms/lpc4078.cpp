00929d8d1e86fe3fca0071a2303743099d355cf5
Khalil Estell <khalil.estell@sjsu.edu>
2025-05-27 20:28:12 -0700

    :recycle: -armcortex -> arm-mcu (#71)

--- demos/platforms/lpc4078.cpp
+++ demos/platforms/lpc4078.cpp
@@ -12,8 +12,8 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+#include <libhal-arm-mcu/dwt_counter.hpp>
 #include <libhal-arm-mcu/lpc40/clock.hpp>
-#include <libhal-armcortex/dwt_counter.hpp>
 
 #include "../resource_list.hpp"
 
